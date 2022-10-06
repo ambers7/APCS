5f2c2cc02b09d7cc4e1f06d8ebd4c8b1f3877974
ambers7 <ambrshen@gmail.com>
2022-10-06 18:55:51 -0400

    Copmlete 10/6/22 classwork (returnCopy)

--- returnCopy.java
+++ returnCopy.java
@@ -15,9 +15,29 @@ public class returnCopy{
         copy += nums[i];
       }
     }
+    return copy;
+  }
+  public static int[] concatArray(int[]ary1,int[]ary2) {
+    int totalLength = ary1.length+ary2.length;
+    int[] total = new int[totalLength];
+    //add ary1 first
+    for (int i = 0;i<ary1.length;i++) {
+      total[i]=ary1[i];
+    }
+    //add ary2
+    for (int i=0;i<ary2.length;i++) {
+      total[i+ary1.length] = ary2[i];
+    }
+    return total;
   }
   public static void main(String[] args) {
+    //test returnCopy
     int[] ary = {1,2,3};
-    System.out.println(returnCopy(ary));
+    System.out.println(aryToString(returnCopy(ary)));
+
+    //test concatArray
+    int[] ary1 = {1,2,3};
+    int[] ary2 = {4,5,6};
+    System.out.println(aryToString(concatArray(ary1,ary2)));
   }
 }
